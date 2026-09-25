415becbe8a1d239a186743fa74538fa4148c2639
Startcpc <startcpc@gmail.com>
2026-09-25 23:04:21 +0300

    Create constants.py

---
+++ src/toolkit/constants.py
@@ -0,0 +1,15 @@
+HELP = """
+Команды:
+
+calc EXPRESSION
+
+Вычислить арифметическое выражение
+
+convert VALUE --from UNIT --to UNIT
+
+Конвертировать единицу измерения
+Примеры:
+  python -m toolkit calc "8 / 2 + 1"
+  python -m toolkit convert 2500 --from cm --to m
+  python -m toolkit --help
+"""
