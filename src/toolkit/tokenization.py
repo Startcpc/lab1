d33cf389de8c0d5ae03cfe0b21f289888abeb97c
Startcpc <startcpc@gmail.com>
2026-09-26 20:55:10 +0300

    regularki tokenization.py

--- src/toolkit/tokenization.py
+++ src/toolkit/tokenization.py
@@ -1,25 +1,7 @@
-def tokenization(task: str) -> list[str]:
-    """Разделяет выражение на числа и арифметические операторы, учитывая унарные знаки."""
-    clean = task.replace(' ', '')
-    tokens = []
-    number = ''
+import re
 
-    for char in clean:
-        if char in '+-':
-            if number:
-                tokens.append(number)
-                number = ''
-            if (not tokens) or tokens[-1] in ['+', '-', '*', '/']:
-                number = char
-            else:
-                tokens.append(char)
-        elif char in '*/':
-            if number:
-                tokens.append(number)
-                number = ''
-            tokens.append(char)
-        else:
-            number += char
-    if number:
-        tokens.append(number)
-    return tokens
+
+def tokenization(task: str) -> list[str]:
+    """Разделяет выражение на токены"""
+    clean = "".join(task.split())
+    return re.findall(r"(?<![\w.])[+-]?(?:\d+(?:\.\d*)?|\.\d+)|[*/]|[+-]|[^\s]", clean)
